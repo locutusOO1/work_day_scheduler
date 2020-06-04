432a24c834459089fcbf8bd0c5d1635858a43ea2
Clark Kent with glasses <61812163+locutusOO1@users.noreply.github.com>
2020-06-04 08:45:42 -0500

    2D array of moment and string to hold schedule

--- Develop/code.js
+++ Develop/code.js
@@ -1,29 +1,22 @@
 var interval;
-var dayTime = moment(9,"HH").format("YYYY-MM-DD");
 var curDayTime = moment();
-var schedule = [[]];
-var block = {
-    mom: moment().format("YYYY-MM-DD"),
-    ev: ""
-}
+var schedule = [];
 var hoursInWorkDay = 9;
 
 function initDay () {
-    // dayTime.hour(9);
     for (var i = 0; i < hoursInWorkDay; i++) {
-        dayTime.add(i,'h');
-        schedule[i].push(dayTime);
+        var dayTime = moment(9+i,"HH");
+        schedule.push([dayTime,""]);
     }
-    for (var i = 0; i< schedule.length; i++) {
-        console.log("i: " + schedule[i][0]);
-    }
-
+    // for (var i = 0; i< schedule.length; i++) {
+    //     console.log("i: " + schedule[i][0].hour()%12);
+    // }
 }
 
 
 
 
-alert(moment().format("YYYY-MM-DD"));
+// alert(moment().format("YYYY-MM-DD"));
 $('#currentDay').text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
 interval = setInterval( function() {
     $('#currentDay').text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
